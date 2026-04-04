44bd01a521e7ace52842c34100cc23426716a99d
Joseph Mokenela <joseph.mokenela@gmail.com>
2026-04-05 01:27:28 +0200

    Initial project commit

---
+++ src/main/java/io/mokenela/transactionaggregator/TransactionAggregatorApplication.java
@@ -0,0 +1,13 @@
+package io.mokenela.transactionaggregator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionAggregatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransactionAggregatorApplication.class, args);
+    }
+
+}
